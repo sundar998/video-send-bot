c817d54f900f6ca8fbc7541b3745e52f19ddf6ae
sundar998 <sundar19921@gmail.com>
2025-06-16 14:11:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-FSUB_CHANNEL_ID = os.getenv("FSUB_CHANNEL_ID")
-VIDEO_DELETE_TIMER = int(os.getenv("VIDEO_DELETE_TIMER", 0))
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MONGO_URI = os.environ.get("MONGO_URI")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+FSUB_CHANNEL_ID = os.environ.get("FSUB_CHANNEL_ID")
+VIDEO_DELETE_TIMER = int(os.environ.get("VIDEO_DELETE_TIMER", 60))  # Default 60s if not set
