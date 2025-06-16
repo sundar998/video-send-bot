a8673e9adf7e2d27f0170d6944f6772fedbfe9ba
sundar998 <sundar19921@gmail.com>
2025-06-16 13:45:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 FSUB_CHANNEL_ID = os.getenv("FSUB_CHANNEL_ID")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+VIDEO_DELETE_TIMER = int(os.getenv("VIDEO_DELETE_TIMER", 0))
